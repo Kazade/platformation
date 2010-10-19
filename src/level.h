eb82ccc868c1a8b5af1f53bff947a1e9fd9e52b3
Luke Benstead <kazade@gmail.com>
2010-10-19 16:47:36 +0100

    Start implementing various tile classes

---
+++ src/level.h
@@ -0,0 +1,36 @@
+#ifndef LEVEL_H_INCLUDED
+#define LEVEL_H_INCLUDED
+
+#include "tile_instance.h"
+#include "gimmick_instance.h"
+#include "entity_instance.h"
+
+class Level {
+public:
+    Level(Tileset* tileset);
+
+    SceneObject* get_selected_object() const;
+
+    TileInstance* spawn_tile_instance(int tile_id, bool select=true);
+    /*GimmickInstance* spawn_gimmick_instance(int gimmick_id);
+    EntityInstance* spawn_entity_instance(int entity_id);*/
+
+    int get_tile_instance_count() const;
+    TileInstance* get_tile_instance_at(int i) const;
+
+    /*int get_entity_instance_count() const;
+    EntityInstance* get_entity_instance_at(int i) const;
+
+    int get_gimmick_instance_count() const;
+    GimmickInstance* get_gimmick_instance_at(int i) const;*/
+
+    bool save(const std::string& filename) const;
+    bool load(const std::string& filename);
+
+private:
+    std::list<TileInstance::ptr> tile_instances_;
+
+};
+
+
+#endif // LEVEL_H_INCLUDED
---
+++ src/tile_instance.h
@@ -0,0 +1,23 @@
+#ifndef TILE_INSTANCE_H_INCLUDED
+#define TILE_INSTANCE_H_INCLUDED
+
+#include <clutter.h>
+
+class Tileset;
+class Tile;
+
+class TileInstance {
+public:
+    TileInstance(Tileset* tileset, int tile_id);
+
+    void set_position() const;
+    void get_position(float& x, float& y);
+
+    ClutterActor* get_actor() const;
+
+private:
+    Tile* tile_;
+    ClutterTexture* actor_;
+};
+
+#endif // TILE_INSTANCE_H_INCLUDED
